5839eaef3650ba300e812a1274b2a5184e73b5e7
MinhyunCheon <90489192+MinhyunCheon@users.noreply.github.com>
2021-11-16 16:03:48 +0900

    Create 직사각형 별찍기.java

---
+++ Programmers/Level 1/직사각형 별찍기.java
@@ -0,0 +1,25 @@
+문제 설명
+이 문제에는 표준 입력으로 두 개의 정수 n과 m이 주어집니다.
+별(*) 문자를 이용해 가로의 길이가 n, 세로의 길이가 m인 직사각형 형태를 출력해보세요.
+
+제한 조건
+n과 m은 각각 1000 이하인 자연수입니다.
+
+
+
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for(int idx1 = 0; idx1 < b; idx1++) {
+            for(int idx2 = 0; idx2 < a; idx2++) {
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+}
